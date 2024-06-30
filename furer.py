d549210b9ac9e4fe67248e87a64320b2158704ce
Lenkly2 <143933790+Lenkly2@users.noreply.github.com>
2024-06-30 13:27:05 +0300

    Add files via upload

--- furer.py
+++ furer.py
@@ -1,6 +1,7 @@
 import pandas as pd
-df = pd.read_csv("GoogleApps.csv")
+# df = pd.read_csv("GoogleApps.csv")
 
+df = pd.read_csv("GooglePlayStore_wild.csv")
 # adolf = df["Content Rating"].value_counts()
 # print(adolf)
 # gitler = adolf["Teen"] / adolf["Everyone 10+"]
@@ -17,5 +18,10 @@ df = pd.read_csv("GoogleApps.csv")
 # comycs = df.groupby(by="Category")["Size"].agg(["min", "max"])
 # print(comycs)
 
-mmm = df[df["Rating"] > 4.5]["Category"].value_counts
-print(mmm["FINANCE"])
+# mmm = df[df["Rating"] > 4.5]["Category"].value_counts
+# print(mmm["FINANCE"])
+
+# print(len(df[pd.isnull(df["Rating"])]))
+# df["Rating"].fillna(1,inplace=True)
+
+print(df[df["Category"]=="TOOLS"]["Size"].value_counts().sort_values())
